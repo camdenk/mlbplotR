93e5e5eccc4bf7592ff946c97d3ad6da841aa185
Camden <camden.kay12@yahoo.com>
2022-02-11 12:56:31 -0500

    Created logo, primary colors, secondary colors, and logo urls lists and saved them in the correct location

---
+++ data-raw/build_logo_list.R
@@ -0,0 +1,21 @@
+# Save raw logos in internal data for more speed
+teams_colors_logos <- load_mlb_teams()
+
+logo_list <- lapply(teams_colors_logos$team_espn_abbr, function(x){
+  url <- teams_colors_logos$team_logo_espn[teams_colors_logos$team_espn_abbr == x]
+  curl::curl_fetch_memory(url)$content
+})
+
+logo_list <- rlang::set_names(logo_list, teams_colors_logos$team_espn_abbr)
+
+primary_colors <- teams_colors_logos$team_color
+names(primary_colors) <- teams_colors_logos$team_espn_abbr
+
+secondary_colors <- teams_colors_logos$team_color2
+names(secondary_colors) <- teams_colors_logos$team_espn_abbr
+
+logo_urls <- teams_colors_logos$team_logo_espn
+names(logo_urls) <- teams_colors_logos$team_espn_abbr
+
+
+usethis::use_data(logo_list, primary_colors, secondary_colors, logo_urls, internal = TRUE, overwrite = TRUE)
