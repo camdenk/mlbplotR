d00ff3298efae4e38c524d9a072b321f4da7ea55
Camden <camden.kay12@yahoo.com>
2022-02-11 12:56:44 -0500

    Initial

---
+++ R/mlbplotR-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @importFrom ggplot2 element_grob
+#' @import grid
+#' @importFrom magrittr %>%
+#' @importFrom rlang .data %||%
+## usethis namespace: end
+NULL
