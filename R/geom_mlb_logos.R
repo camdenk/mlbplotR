e05d95b559f079b769f9f9a16bf9e45cf4adede7
Camden <camden.kay12@yahoo.com>
2022-02-11 12:57:21 -0500

    Reverted back to colour

--- R/geom_mlb_logos.R
+++ R/geom_mlb_logos.R
@@ -12,7 +12,7 @@
 #'   \item{**y**}{ - The y-coordinate.}
 #'   \item{**team_abbr**}{ - The team abbreviation. Should use ESPN's abbreviation labels.}
 #'   \item{`alpha = NULL`}{ - The alpha channel, i.e. transparency level, as a numerical value between 0 and 1.}
-#'   \item{`color = NULL`}{ - The image will be colorized with this color. Use the special character `"b/w"` to set it to black and white. For more information on valid color names in ggplot2 see <https://ggplot2.tidyverse.org/articles/ggplot2-specs.html?q=colour#colour-and-fill>}
+#'   \item{`colour = NULL`}{ - The image will be colourized with this colour. Use the special character `"b/w"` to set it to black and white. For more information on valid colour names in ggplot2 see <https://ggplot2.tidyverse.org/articles/ggplot2-specs.html?q=colour#colour-and-fill>}
 #'   \item{`angle = 0`}{ - The angle of the image as a numerical value between 0° and 360°.}
 #'   \item{`hjust = 0.5`}{ - The horizontal adjustment relative to the given x coordinate. Must be a numerical value between 0 and 1.}
 #'   \item{`vjust = 0.5`}{ - The vertical adjustment relative to the given y coordinate. Must be a numerical value between 0 and 1.}
@@ -49,8 +49,8 @@
 #' # keep alpha == 1 for all teams including an "A"
 #' matches <- grepl("A", team_abbr)
 #' df$alpha <- ifelse(matches, 1, 0.2)
-#' # also set a custom fill color for the non "A" teams
-#' df$color <- ifelse(matches, NA, "gray")
+#' # also set a custom fill colour for the non "A" teams
+#' df$colour <- ifelse(matches, NA, "gray")
 #'
 #' # scatterplot of all logos
 #' ggplot(df, aes(x = a, y = b)) +
@@ -67,19 +67,19 @@
 #'   scale_alpha_identity() +
 #'   theme_void()
 #'
-#' # apply alpha and color via an aesthetic from inside the dataset `df`
+#' # apply alpha and colour via an aesthetic from inside the dataset `df`
 #' # please note that you have to add scale_alpha_identity() as well as
-#' # scale_color_identity() to use the alpha and color values in your dataset!
+#' # scale_colour_identity() to use the alpha and colour values in your dataset!
 #' ggplot(df, aes(x = a, y = b)) +
-#'   geom_mlb_logos(aes(team_abbr = teams, alpha = alpha, color = color), width = 0.075) +
+#'   geom_mlb_logos(aes(team_abbr = teams, alpha = alpha, colour = colour), width = 0.075) +
 #'   geom_label(aes(label = teams), nudge_y = -0.35, alpha = 0.5) +
 #'   scale_alpha_identity() +
-#'   scale_color_identity() +
+#'   scale_colour_identity() +
 #'   theme_void()
 #'
 #' # apply alpha as constant for all logos
 #' ggplot(df, aes(x = a, y = b)) +
-#'   geom_nfl_logos(aes(team_abbr = teams), width = 0.075, alpha = 0.6) +
+#'   geom_mlb_logos(aes(team_abbr = teams), width = 0.075, alpha = 0.6) +
 #'   geom_label(aes(label = teams), nudge_y = -0.35, alpha = 0.5) +
 #'   theme_void()
 #'
@@ -120,13 +120,13 @@ GeomMLBlogo <- ggplot2::ggproto(
   required_aes = c("x", "y", "team_abbr"),
   # non_missing_aes = c(""),
   default_aes = ggplot2::aes(
-    alpha = NULL, color = NULL, angle = 0, hjust = 0.5,
+    alpha = NULL, colour = NULL, angle = 0, hjust = 0.5,
     vjust = 0.5, width = 1.0, height = 1.0
   ),
   draw_panel = function(data, panel_params, coord, na.rm = FALSE) {
     data <- coord$transform(data, panel_params)
 
-    grobs <- lapply(seq_along(data$team_abbr), build_grobs, alpha = data$alpha, color = data$color, data = data, type = "teams")
+    grobs <- lapply(seq_along(data$team_abbr), build_grobs, alpha = data$alpha, colour = data$colour, data = data, type = "teams")
 
     class(grobs) <- "gList"
 
