6bcad0fd04d0777e6bde80a5ea3d64cf20aebf05
Camden <camden.kay12@yahoo.com>
2022-02-11 10:39:21 -0500

    Function to load in abbreviations, logos, and color data

---
+++ R/load_data.R
@@ -0,0 +1,19 @@
+#' Load MLB Team Colors, and Logos
+#'
+#' @description Loads team colors, and logos - useful for plots!
+#'
+#' @examples
+#' \donttest{
+#'   load__mlb_teams()
+#' }
+#'
+#' @return A tibble of team-level abbreviations, image URLs, and hex color codes.
+#'
+#' @seealso Issues with this data should be filed here: <https://github.com/camdenk/mlbplotR>
+#'
+#' @export
+load_mlb_teams <- function(){
+  out <- readRDS("./raw-data/MLB_Colors_Logos.rds")
+  #class(out) <- c("tbl_df","tbl","data.table","data.frame")
+  out
+}
